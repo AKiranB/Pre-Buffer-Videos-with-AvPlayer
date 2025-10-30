76671a6495583255a87a89dda2201a3a0488cde7
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-10-30 07:23:36 +0100

    Initial Commit

---
+++ AvQueuePlayer testTests/AvQueuePlayer_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  AvQueuePlayer_testTests.swift
+//  AvQueuePlayer testTests
+//
+//  Created by Kiran Boyle on 30.10.25.
+//
+
+import Testing
+@testable import AvQueuePlayer_test
+
+struct AvQueuePlayer_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
