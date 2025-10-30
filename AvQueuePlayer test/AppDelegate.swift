1eed8e5cad16f3fd0f8c6dfe4cb56a5c9d1b660b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2025-10-30 10:47:30 +0100

    show player, create VideoPlayer class

--- AvQueuePlayer test/AppDelegate.swift
+++ AvQueuePlayer test/AppDelegate.swift
@@ -1,36 +1,71 @@
 //
 //  AppDelegate.swift
-//  AvQueuePlayer test
+//  AvPlayertest
 //
 //  Created by Kiran Boyle on 30.10.25.
 //
 
+import AVFoundation
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        let w = UIWindow(frame: UIScreen.main.bounds)
+        let vc = UIViewController()
+        vc.view.backgroundColor = .green
+        w.rootViewController = PlayerViewController()
+        w.makeKeyAndVisible()
+        window = w
+        print("BOOT OK")
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
 }
 
+
+final class VideoPlayerView: UIView {
+    override class var layerClass: AnyClass { AVPlayerLayer.self }
+    private var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
+    private let player = AVPlayer()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+        player.automaticallyWaitsToMinimizeStalling = true
+        playerLayer.player = player
+        playerLayer.videoGravity = .resizeAspect
+    }
+    required init?(coder: NSCoder) { super.init(coder: coder) }
+
+    func set(item: AVPlayerItem) { player.replaceCurrentItem(with: item) }
+    func play() { player.play() }
+    func pause() { player.pause() }
+}
+
+
+final class PlayerView: UIView {
+    override class var layerClass: AnyClass { AVPlayerLayer.self }
+    var playerLayer: AVPlayerLayer { layer as! AVPlayerLayer }
+}
+
+final class PlayerViewController: UIViewController {
+    
+    let videoPlayerView = VideoPlayerView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+        view.addSubview(videoPlayerView)
+        let url = URL(string:"https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8")!
+        
+        let item = AVPlayerItem(url: url)
+        videoPlayerView.set(item: item)
+        videoPlayerView.play()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        videoPlayerView.frame = view.bounds
+    }
+}
